f6dff3de23ce7873750428a94edc5f1f0f1d6c29
Mounika Sharan <mounikar523@gmail.com>
2022-10-01 20:13:33 +0530

    added asset allocation

---
+++ Models/AssetAllocation.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BVPortalApi.Models
+{
+    public class AssetAllocation
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public int AllocatedById { get; set; }
+        public string? AllocatedBy { get; set; }
+        public int AllocatedToId { get; set; }
+        public string? AllocatedTo { get; set; }
+        public string? AllocatedDate{ get; set; }
+        public string? ReturnDate { get; set; }
+        public string? Status { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
