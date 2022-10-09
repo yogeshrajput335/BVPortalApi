ab0c01f8b130480946423b23c8ca6db4321aae9a
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2022-10-09 11:43:11 +0530

    Updated leave dto

--- DTO/LeaveTypeDTO.cs
+++ DTO/LeaveTypeDTO.cs
@@ -11,6 +11,5 @@ namespace BVPortalApi.DTO
         public string? Type { get; set; }
         public string? Description { get; set; }
         public string? Status { get; set; }
-        public bool IsActive { get; set; }
     }
 }
