e5b01443af8282f5b952d9b87d2946637392e6ec
Mounika Sharan <mounikar523@gmail.com>
2022-10-23 10:59:20 +0530

    updated candidatedto

--- DTO/CandidateDTO.cs
+++ DTO/CandidateDTO.cs
@@ -13,6 +13,5 @@ namespace BVPortalApi.DTO
         public string? PhoneNo{ get; set; }
         public string? Email { get; set; }
         public string? Status { get; set; }
-        public bool IsActive { get; set; }
     }
 }
