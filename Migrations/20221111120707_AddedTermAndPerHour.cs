ce5850c296c21cdb384107fafd37bc22833f20ba
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2022-11-11 17:55:35 +0530

    Added Term And PerHour

---
+++ Migrations/20221111120707_AddedTermAndPerHour.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BVPortalApi.Migrations
+{
+    public partial class AddedTermAndPerHour : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientTerm",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    TermText = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Term = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientTerm", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientTerm_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ClientTermHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    OldTermText = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    OldTerm = table.Column<int>(type: "int", nullable: false),
+                    NewTermText = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NewTerm = table.Column<int>(type: "int", nullable: false),
+                    ReasonForChange = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ChangeDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ChangeBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientTermHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientTermHistory_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "EmpClientPerHour",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    PerHour = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpClientPerHour", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmpClientPerHour_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmpClientPerHour_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "EmpClientPerHourHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    OldPerHour = table.Column<float>(type: "float", nullable: false),
+                    NewPerHour = table.Column<float>(type: "float", nullable: false),
+                    ReasonForChange = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ChangeDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ChangeBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpClientPerHourHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmpClientPerHourHistory_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmpClientPerHourHistory_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientTerm_ClientId",
+                table: "ClientTerm",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientTermHistory_ClientId",
+                table: "ClientTermHistory",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpClientPerHour_ClientId",
+                table: "EmpClientPerHour",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpClientPerHour_EmployeeId",
+                table: "EmpClientPerHour",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpClientPerHourHistory_ClientId",
+                table: "EmpClientPerHourHistory",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpClientPerHourHistory_EmployeeId",
+                table: "EmpClientPerHourHistory",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientTerm");
+
+            migrationBuilder.DropTable(
+                name: "ClientTermHistory");
+
+            migrationBuilder.DropTable(
+                name: "EmpClientPerHour");
+
+            migrationBuilder.DropTable(
+                name: "EmpClientPerHourHistory");
+        }
+    }
+}
