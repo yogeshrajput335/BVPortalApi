c0a518633e59d07e6cca1b4546c28e1e6f36dca1
Mounika Sharan <mounikar523@gmail.com>
2022-11-13 10:03:25 +0530

    updated client term and emp client

---
+++ DTO/EmpClientPerHourDTO .cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BVPortalApi.DTO
+{
+    public class EmpClientPerHourDTO
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int ClientId { get; set; }
+        public float PerHour { get; set; }
+    }
+}
